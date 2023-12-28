279303c76fe05e845067125c596502e8e4e6b19d
malexj <alexmaximovj@gmail.com>
2023-12-28 21:44:33 +0200

    Fix

---
+++ src/test/java/com/malexj/Test.java
@@ -0,0 +1,11 @@
+package com.malexj;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Test {
+
+    @org.junit.jupiter.api.Test
+    void test() {
+        assertEquals(1, 1);
+    }
+}
