e6e3091708aaee3f00e53bca712a7b08c82497c7
malexj <alexmaximovj@gmail.com>
2023-12-28 22:33:39 +0200

    Add prototype

--- src/main/java/com/malexj/training_course/bean_scope/bean/Waiter.java
+++ src/main/java/com/malexj/training_course/bean_scope/bean/Waiter.java
@@ -1,8 +1,13 @@
 package com.malexj.training_course.bean_scope.bean;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
-public class Waiter {
-
-}
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class Waiter {}
