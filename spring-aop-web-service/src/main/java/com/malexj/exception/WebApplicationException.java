16b220247811756ecca54a0271b95f8c7b9db733
malexj <alexmaximovj@gmail.com>
2024-01-25 15:49:03 +0200

    Remove springboot project from core

--- spring-aop-web-service/src/main/java/com/malexj/exception/WebApplicationException.java
+++
@@ -1,8 +0,0 @@
-package com.malexj.exception;
-
-public class WebApplicationException extends RuntimeException {
-
-  public WebApplicationException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
--- spring-core/src/main/java/com/malexj/training_course/event_listener/custom/EventCustomApp.java
+++ spring-core/src/main/java/com/malexj/training_course/event_listener/custom/EventCustomApp.java
@@ -2,15 +2,18 @@ package com.malexj.training_course.event_listener.custom;
 
 import com.malexj.training_course.event_listener.custom.bean.Restaurant;
 import com.malexj.training_course.event_listener.custom.config.EventCustomConfig;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class EventCustomApp {
-    public static void main(String[] args){
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(EventCustomConfig.class);
-        var restaurant = ctx.getBean(Restaurant.class);
-        restaurant.placeOrder("Milk");
-        restaurant.placeOrder("Salad");
-        restaurant.placeOrder("Tea");
-    }
+
+  @Test
+  public void run() {
+    ApplicationContext ctx = new AnnotationConfigApplicationContext(EventCustomConfig.class);
+    var restaurant = ctx.getBean(Restaurant.class);
+    restaurant.placeOrder("Milk");
+    restaurant.placeOrder("Salad");
+    restaurant.placeOrder("Tea");
+  }
 }
